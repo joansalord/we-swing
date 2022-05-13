46233d3de240b777b351b071c3a4e3ef572fd548
Joan <daw1jsalordribo@iesjoanramis.org>
2022-05-13 09:41:59 +0200

    Added user search feature & fetch on profile

--- src/pages/main/Tab2.tsx
+++ src/pages/main/Tab2.tsx
@@ -1,23 +1,81 @@
-import { IonContent, IonHeader, IonIcon, IonLabel, IonPage, IonTabBar, IonTabButton, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonButton, IonCol, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonNote, IonPage, IonRow, IonTabBar, IonTabButton, IonTitle, IonToolbar } from '@ionic/react';
 import { calendar, search, person } from 'ionicons/icons';
+import { useState } from 'react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+
+  const [name, setName] = useState<string>();
+
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState<{
+    username: string,
+    fullName: string,
+    email: string,
+    gender: string,
+    country: string,
+    language: string,
+    date: string
+  }[]>([]);
+
+  function searchUsers() {
+    //Fetch by name.
+    fetch("http://localhost:8080/process/getUsers")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result);
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setError(error);
+          console.log(error);
+        }
+      )
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Search</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className='mainDiv'>
+          <IonRow>
+            <IonCol size='8'>
+              <IonInput value={name} placeholder="Enter name..." onIonChange={e => setName(e.detail.value!)}></IonInput>
+            </IonCol>
+            <IonCol size='4'>
+              <IonButton onClick={searchUsers}>Search</IonButton>
+            </IonCol>
+          </IonRow>
+          <IonList>
+            {items.map(item => (
+              //<IonItem routerLink={`profile/${item.username}`}></IonItem>
+              <IonItem routerLink={'profile/username'}>
+                <IonAvatar slot="start">
+                  <img src="../../resources/appIcon.png" />
+                </IonAvatar>
+                <IonLabel>
+                  <h1 className= "title" >{item.username}</h1>
+                  <h3>Name: {item.fullName}</h3>
+                  <h3>Email: {item.email}</h3>
+                  <IonNote>Gender: {item.gender}</IonNote>
+                  <p>Country: {item.country}</p>
+                  <p>Language: {item.language}</p>
+                  <p>Date: {item.date}</p>
+                </IonLabel>
+              </IonItem>
+            ))}
+          </IonList>
+        </div>
+
       </IonContent>
       <IonTabBar slot="bottom">
           <IonTabButton tab="events" href="/events">
--- src/pages/main/profile/Profile.tsx
+++ src/pages/main/profile/Profile.tsx
@@ -1,6 +1,6 @@
 import { IonAvatar, IonButton, IonButtons, IonCol, IonContent, IonHeader, IonIcon, IonLabel, IonLoading, IonPage, IonRow, IonTabBar, IonTabButton, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import { calendar, ellipse, person, search, star } from 'ionicons/icons';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { logoutUser } from '../../../firebaseConfig';
@@ -8,6 +8,17 @@ import './Profile.css';
 
 const Profile: React.FC = () => {
   const username = useSelector((state: any) => state.user.username)
+  const [error, setError] = useState(null);
+
+  const [items, setItems] = useState<{
+    fullName: string;
+    email: string;
+    gender: string;
+    country: string;
+    language: string;
+    description: string;
+    date: string;
+  }>();
 
   const history = useHistory()
   const [busy, setBusy] = useState(false)
@@ -20,7 +31,25 @@ const Profile: React.FC = () => {
 
   function edit() {
     history.replace('/editProfile')
-}
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/process/profile")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result);
+          console.log(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setError(error);
+          console.log(error);
+        }
+      )
+  }, [])
 
   return (
     <IonPage>
@@ -99,30 +128,36 @@ const Profile: React.FC = () => {
           <IonRow>
             <IonCol size='8'>
               <IonLabel class='profileLabel'>Full name</IonLabel>
+              <p>{items?.fullName}</p>
               <p>Joan Salord Ribó</p>
             </IonCol>
             <IonCol size='4'>
               <IonLabel class='profileLabel'>Birth date</IonLabel>
+              <p>{items?.date}</p>
               <p>21/08/2000</p>
             </IonCol>
           </IonRow>
           <IonRow>
           <IonCol>
               <IonLabel class='profileLabel'>Email</IonLabel>
+              <p>{items?.email}</p>
               <p>a21joasalrib@inspedralbes.cat</p>
             </IonCol>
           </IonRow>
           <IonRow> 
             <IonCol size='4'>
-              <IonLabel class='profileLabel'>Sex</IonLabel>
+              <IonLabel class='profileLabel'>Gender</IonLabel>
+              <p>{items?.gender}</p>
               <p>Please</p>
             </IonCol>
             <IonCol size='4'>
               <IonLabel class='profileLabel'>Country</IonLabel>
+              <p>{items?.country}</p>
               <p>Spain</p>
             </IonCol>
             <IonCol size='4'>
               <IonLabel class='profileLabel'>Language</IonLabel>
+              <p>{items?.language}</p>
               <p>Spanish</p>
             </IonCol>
           </IonRow>
@@ -130,6 +165,7 @@ const Profile: React.FC = () => {
             <IonCol>
               <IonLabel class='profileLabel'>Description</IonLabel>
               <IonTextarea >
+              <p>{items?.description}</p>
                 <p className='descriptionText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam molestie sem nec nibh congue gravida. Curabitur quis tincidunt lacus. Proin convallis at dolor non malesuada. Maecenas bibendum nulla sit amet diam sagittis sollicitudin. Integer pulvinar lobortis neque, ut venenatis risus tempus id. Aliquam tristique commodo rutrum. Vestibulum volutpat vehicula arcu, vel gravida sapien sodales eget. Aenean ac cursus purus. Ut vitae quam eu mi iaculis bibendum eu ac arcu.</p>
               </IonTextarea>
             </IonCol>
